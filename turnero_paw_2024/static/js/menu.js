9d8a4d50a7a7b1b3e0b6ef01889ce19a9cacd711
iarzaesteban <iarzaesteban94@gmail.com>
2024-04-07 02:17:58 -0300

    si abrimos un menu y queremos abrir el otro, cerramos el que esta abiero y abrimos el que se esta clickeando

--- turnero_paw_2024/static/js/menu.js
+++ turnero_paw_2024/static/js/menu.js
@@ -1,11 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const buttonsOutsideMenu = document.querySelectorAll('button:not(#header-avatar-btn)');
+    const linksOutsideMenu = document.querySelectorAll('a.acept-btn, a.cancel-btn');
+    document.addEventListener('click', function(event) {
+        if (event.target !== headerAvatarBtn && event.target !== _toggle && 
+            !headerAvatarBtn.contains(event.target) && !_toggle.contains(event.target)) {
+            closeMenus();
+            enableButtonsAndLinks();
+        }
+    });
+    
     _toggle.onclick = () => {
         _items.classList.toggle("open");
         _toggle.classList.toggle("close");
+        disableButtonsAndLinks();
+        headerAvatarBtn.classList.remove('active');
+        navItemsUser.classList.remove('active');
+    }
+    
+    const headerAvatarBtn = document.getElementById('header-avatar-btn');
+    const navItemsUser = document.querySelector('.nav-items-user');
+
+    function closeMenus() {
+        _items.classList.remove("open");
+        _toggle.classList.remove("close");
+        if (headerAvatarBtn) {
+            headerAvatarBtn.classList.remove('active');
+        }
+        if (navItemsUser) {
+            navItemsUser.classList.remove('active');
+        }
     }
 
     document.getElementById('header-avatar-btn').addEventListener('click', function() {
         this.classList.toggle('active');
+        disableButtonsAndLinks();
+        _items.classList.remove("open");
+        _toggle.classList.remove("close");
     });
 
     const userAvatar = document.getElementById('user-avatar');
@@ -13,7 +43,24 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const userAvatarUrl = '/get-user-avatar-url';
     
+    function disableButtonsAndLinks() {
+        buttonsOutsideMenu.forEach(button => {
+            button.style.pointerEvents = "none";
+        });
+        linksOutsideMenu.forEach(link => {
+            link.style.pointerEvents = "none";
+        });
+    }
 
+    function enableButtonsAndLinks() {
+        buttonsOutsideMenu.forEach(button => {
+            button.style.pointerEvents = "auto";
+        });
+        linksOutsideMenu.forEach(link => {
+            link.style.pointerEvents = "auto";
+        });
+    }
+    
     fetch(userAvatarUrl)
         .then(response => {
             if (response.ok) {
