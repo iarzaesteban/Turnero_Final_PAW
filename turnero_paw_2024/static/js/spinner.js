46ae97118fd12bfe626e5561e2afe7e4a7c36bd4
iarzaesteban <iarzaesteban94@gmail.com>
2024-04-08 01:56:15 -0300

    agregamos estilos pantalla de Enviar mail y spinner en boton enviar mail

---
+++ turnero_paw_2024/static/js/spinner.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('send-email-form');
+    const sendEmailBtn = document.getElementById('send-email-form__btn');
+    const spinner = document.getElementById('spinner');
+    const textButton = document.getElementById('text-btn');
+
+    function areFieldsFilled() {
+        const email = form.querySelector('input[name="email"]').value.trim();
+        const subject = form.querySelector('input[name="subject"]').value.trim();
+        const message = form.querySelector('textarea[name="message"]').value.trim();
+        return email !== '' && subject !== '' && message !== '';
+    }
+
+    spinner.classList.add("spinner-hidden");
+
+    sendEmailBtn.addEventListener('click', function() {
+        if (areFieldsFilled()) {
+            textButton.style.display = "none";
+            spinner.classList.remove("spinner-hidden");
+        }
+    });
+});
